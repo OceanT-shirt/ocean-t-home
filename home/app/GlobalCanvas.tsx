3dd19e4aa0354b3fe6af7a95428086511edf7131
OceanT-shirt <stylish.trousers@gmail.com>
2022-11-05 23:50:41 +0900

    added: rotation

--- home/app/GlobalCanvas.tsx
+++ home/app/GlobalCanvas.tsx
@@ -3,6 +3,7 @@
 import { Canvas, useFrame } from '@react-three/fiber';
 import * as THREE from 'three'
 import { useRef, useState} from "react";
+import Image from 'next/image';
 
 function Mesh() {
    const ref = useRef<THREE.Mesh>(null!);
@@ -24,7 +25,40 @@ function Mesh() {
                 </mesh>
 
        );
+}
 
+function InfoDisplay() {
+    const ref = useRef<THREE.Mesh>(null!);
+    const [isHovered, setIsHovered] = useState(false);
+    const [isRotationReverse, setIsRotationReverse] = useState(false);
+    const [video] = useState(() => Object.assign(<Image alt={"hoge"} src={"./q-zip.png"} /> ))
+    useFrame(() => {
+        if (ref.current && !isHovered) {
+            if (ref.current.rotation.y == Math.PI/2) setIsRotationReverse(true)
+            if (ref.current.rotation.y == Math.PI*-1/2) setIsRotationReverse(false)
+            if(isRotationReverse) {
+                ref.current.rotation.y += 0.01
+            } else {
+                ref.current.rotation.y -= 0.01
+            }
+        }
+    })
+
+    return (
+        <mesh
+            ref={ref}
+            onPointerOver={() => setIsHovered(true)}
+            onPointerOut={() => setIsHovered(false)}
+            scale={isHovered ? [8, 6, .2] : [4, 3, .1]}
+            rotation={isHovered ? [0, 0, 0] : [0, 7 * Math.PI / 4, 0]}
+        >
+            <planeGeometry />
+            <meshBasicMaterial toneMapped={false}>
+                <videoTexture attach="map" args={[video]} encoding={THREE.sRGBEncoding} />
+            </meshBasicMaterial>
+        </mesh>
+
+    );
 }
 
 export default function GlobalCanvas() {
@@ -34,7 +68,8 @@ export default function GlobalCanvas() {
                 <color attach="background" args={[0xf5f3fd]} />
                 <ambientLight intensity={0.5} />
                 <directionalLight intensity={0.5} position={[-10, 10, 10]} />
-                <Mesh />
+                {/*<Mesh />*/}
+                <InfoDisplay />
             </Canvas>
         </>
     );
