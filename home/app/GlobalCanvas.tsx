3ede1c35f647cc5a07a2224845f83d6774a7c177
OceanT-shirt <stylish.trousers@gmail.com>
2022-11-06 02:24:07 +0900

    changed: bg clr

--- home/app/GlobalCanvas.tsx
+++ home/app/GlobalCanvas.tsx
@@ -3,29 +3,7 @@
 import { Canvas, useFrame } from '@react-three/fiber';
 import * as THREE from 'three'
 import {useEffect, useRef, useState} from "react";
-import ReactPlayer from 'react-player';
 
-function Mesh() {
-   const ref = useRef<THREE.Mesh>(null!);
-   const [isHovered, setIsHovered] = useState(false);
-   useFrame(() => {
-       if (ref.current) {
-       ref.current.rotation.x += 0.01;
-       ref.current.rotation.y += 0.01;
-       }
-   })
-       return (
-                <mesh
-                    ref={ref}
-                    onPointerOver={() => setIsHovered(true)}
-                    onPointerOut={() => setIsHovered(false)}
-                >
-                    <torusBufferGeometry args={isHovered ? [2, 1, 20, 50] : [1.5, 0.75, 20, 50]} />
-                    <meshLambertMaterial color={isHovered ? 0x44c2b5 : 0x0078e6} />
-                </mesh>
-
-       );
-}
 
 function InfoDisplay() {
     const ref = useRef<THREE.Mesh>(null!);
@@ -34,7 +12,7 @@ function InfoDisplay() {
     useEffect(() => void video.play(), [video])
     useFrame((state) => {
         if (ref.current && !isHovered) {
-            ref.current.rotation.y = Math.PI * Math.sin(state.clock.getElapsedTime()/2)*2/5
+            // ref.current.rotation.y = Math.PI * Math.sin(state.clock.getElapsedTime()/2)*2/5
         }
     })
 
@@ -43,8 +21,9 @@ function InfoDisplay() {
             ref={ref}
             onPointerOver={() => setIsHovered(true)}
             onPointerOut={() => setIsHovered(false)}
-            scale={isHovered ? [8, 6, .2] : [4, 3, .1]}
-            rotation={isHovered ? [0, 0, 0] : [0, 7 * Math.PI / 4, 0]}
+            scale={[8, 4.5, .1]}
+            rotation={[0, 0, 0]}
+            position={[0, 0.7, 0]}
         >
             <planeGeometry />
             <meshBasicMaterial toneMapped={false}>
@@ -59,10 +38,9 @@ export default function GlobalCanvas() {
     return (
         <>
             <Canvas dpr={2} shadows={true}>
-                <color attach="background" args={[0xf5f3fd]} />
-                <ambientLight intensity={0.5} />
-                <directionalLight intensity={0.5} position={[-10, 10, 10]} />
-                {/*<Mesh />*/}
+                <color attach="background" args={[0x383838]} />
+                {/*<ambientLight intensity={0.5} />*/}
+                {/*<directionalLight intensity={0.5} position={[-10, 10, 10]} />*/}
                 <InfoDisplay />
             </Canvas>
         </>
