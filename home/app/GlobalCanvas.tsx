ef2c017c8f27defd405febb71d66d835d18a227a
OceanT-shirt <stylish.trousers@gmail.com>
2022-11-07 02:24:03 +0900

    added: water comment

--- home/app/GlobalCanvas.tsx
+++ home/app/GlobalCanvas.tsx
@@ -1,10 +1,12 @@
 'use client';
 
-import { Canvas, useFrame } from '@react-three/fiber';
+import {Canvas, extend, useFrame, useLoader, useThree} from '@react-three/fiber';
 import * as THREE from 'three'
-import {useEffect, useRef, useState} from "react";
-// import {Effects} from "./Effect";
+import {useEffect, useMemo, useRef, useState} from "react";
+// TODO issue with importing react-three/postprocessing
+// import {EffectComposer} from "@react-three/postprocessing";
 // import {RoundedBox} from "@react-three/drei";
+// import { Water } from 'three-stdlib'
 
 
 function InfoDisplay() {
@@ -36,23 +38,98 @@ function InfoDisplay() {
     );
 }
 
-const Plane = ({ color }: {color: string}) => (
+const Plane = ({ color, position }: {color: string, position: [number, number, number]}) => (
     <mesh
-        position={[0, -2.5, 0]}
+        position={position}
     >
         <boxGeometry args={[12, .2, 2]} />
-        <meshStandardMaterial color={color} envMapIntensity={0.5} roughness={0} metalness={0} />
+        <meshLambertMaterial color={color} reflectivity={0.5} />
     </mesh>
 )
+
+// extend({ Water })
+//
+// function Ocean() {
+//     const ref = useRef()
+//     const gl = useThree((state) => state.gl)
+//     const waterNormals = useLoader(THREE.TextureLoader, '/waternormals.jpeg')
+//     waterNormals.wrapS = waterNormals.wrapT = THREE.RepeatWrapping
+//     const geom = useMemo(() => new THREE.PlaneGeometry(10000, 10000), [])
+//     const config = useMemo(
+//         () => ({
+//             textureWidth: 512,
+//             textureHeight: 512,
+//             waterNormals,
+//             sunDirection: new THREE.Vector3(),
+//             sunColor: 0xffffff,
+//             waterColor: 0x001e0f,
+//             distortionScale: 3.7,
+//             fog: false,
+//             format: gl.encoding
+//         }),
+//         [waterNormals]
+//     )
+//     useFrame((state, delta) => (ref.current.material.uniforms.time.value += delta))
+//     return <water ref={ref} args={[geom, config]} rotation-x={-Math.PI / 2} />
+// }
+
+
+// export function Effects() {
+//     // const props = useControls({
+//     //     temporalResolve: true,
+//     //     STRETCH_MISSED_RAYS: true,
+//     //     USE_MRT: true,
+//     //     USE_NORMALMAP: true,
+//     //     USE_ROUGHNESSMAP: true,
+//     //     ENABLE_JITTERING: true,
+//     //     ENABLE_BLUR: true,
+//     //     temporalResolveMix: { value: 0.9, min: 0, max: 1 },
+//     //     temporalResolveCorrectionMix: { value: 0.25, min: 0, max: 1 },
+//     //     maxSamples: { value: 0, min: 0, max: 1 },
+//     //     resolutionScale: { value: 1, min: 0, max: 1 },
+//     //     blurMix: { value: 0.5, min: 0, max: 1 },
+//     //     blurKernelSize: { value: 8, min: 0, max: 8 },
+//     //     blurSharpness: { value: 0.5, min: 0, max: 1 },
+//     //     rayStep: { value: 0.3, min: 0, max: 1 },
+//     //     intensity: { value: 1, min: 0, max: 5 },
+//     //     maxRoughness: { value: 0.1, min: 0, max: 1 },
+//     //     jitter: { value: 0.7, min: 0, max: 5 },
+//     //     jitterSpread: { value: 0.45, min: 0, max: 1 },
+//     //     jitterRough: { value: 0.1, min: 0, max: 1 },
+//     //     roughnessFadeOut: { value: 1, min: 0, max: 1 },
+//     //     rayFadeOut: { value: 0, min: 0, max: 1 },
+//     //     MAX_STEPS: { value: 20, min: 0, max: 20 },
+//     //     NUM_BINARY_SEARCH_STEPS: { value: 5, min: 0, max: 10 },
+//     //     maxDepthDifference: { value: 3, min: 0, max: 10 },
+//     //     maxDepth: { value: 1, min: 0, max: 1 },
+//     //     thickness: { value: 10, min: 0, max: 10 },
+//     //     ior: { value: 1.45, min: 0, max: 2 }
+//     // })
+//     const props = {
+//             temporalResolve: true,
+//             STRETCH_MISSED_RAYS: true,
+//             USE_MRT: true,
+//             USE_NORMALMAP: true,
+//             USE_ROUGHNESSMAP: true,
+//             ENABLE_JITTERING: true,
+//             ENABLE_BLUR: true,
+//     }
+//     return (
+//             <EffectComposer disableNormalPass>
+//                 {/*<SSR {...props} />*/}
+//             </EffectComposer>
+//     )
+// }
 
 export default function GlobalCanvas() {
     return (
         <>
             <Canvas dpr={2} shadows={true}>
-                <color attach="background" args={[0x111111]} />
+                <color attach="background" args={[0xefefef]} />
                 <ambientLight intensity={0.02} />
-                {/*<directionalLight intensity={0.5} position={[-10, 10, 10]} />*/}
-                <Plane color={'white'} />
+                <directionalLight intensity={0.5} position={[-10, 10, 10]} />
+                <Plane color={'skyblue'} position={[0, -2.5, 0]} />
+                <Plane color={'skyblue'} position={[0, 2.5, 0]} />
                 <InfoDisplay />
                 {/*<Effects />*/}
             </Canvas>
