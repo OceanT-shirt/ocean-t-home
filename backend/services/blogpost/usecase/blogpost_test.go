0a534ffe32bea4f6965fa03f355188e40d413ebc
OceanT-shirt <stylish.trousers@gmail.com>
2022-11-23 08:04:52 +0900

    added: sample tests

--- backend/services/blogpost/usecase/blogpost_test.go
+++ backend/services/blogpost/usecase/blogpost_test.go
@@ -1,10 +1,10 @@
 package usecase
 
 import (
+	"errors"
 	"github.com/OceanT-shirt/ocean-t-home/services/blogpost/model"
 	"github.com/OceanT-shirt/ocean-t-home/services/blogpost/usecase/mock_repo"
 	"github.com/golang/mock/gomock"
-	"gorm.io/gorm"
 	"reflect"
 	"testing"
 )
@@ -14,6 +14,11 @@ func getMockRepo(t *testing.T) *mock_repo.MockRepo {
 	defer ctrl.Finish()
 	mr := mock_repo.NewMockRepo(ctrl)
 	return mr
+}
+
+func getMockBlogPost(title string) *model.BlogPost {
+	p := model.CreateBlogPost(title, "fuga")
+	return &p
 }
 
 func TestBlogPostUseCase_Delete(t *testing.T) {
@@ -42,6 +47,17 @@ func TestBlogPostUseCase_Delete(t *testing.T) {
 				m.EXPECT().Delete("1").Return(nil)
 			},
 		},
+		{
+			name:   "failure",
+			fields: fields{},
+			args: args{
+				id: "invalid",
+			},
+			wantErr: true,
+			prepareMockFn: func(m *mock_repo.MockRepo) {
+				m.EXPECT().Delete("invalid").Return(errors.New("err"))
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -58,22 +74,39 @@ func TestBlogPostUseCase_Delete(t *testing.T) {
 }
 
 func TestBlogPostUseCase_GetAll(t *testing.T) {
-	type fields struct {
-		repo Repo
-	}
+	mockBlog1 := getMockBlogPost("h")
+	mockBlog2 := getMockBlogPost("2")
+	mockBlogs := []model.BlogPost{*mockBlog1, *mockBlog2}
 	tests := []struct {
-		name    string
-		fields  fields
-		want    *[]model.BlogPost
-		wantErr bool
+		name          string
+		want          *[]model.BlogPost
+		wantErr       bool
+		prepareMockFn func(m *mock_repo.MockRepo)
 	}{
-		// TODO: Add test cases.
+		{
+			name:    "success",
+			want:    &mockBlogs,
+			wantErr: false,
+			prepareMockFn: func(m *mock_repo.MockRepo) {
+				m.EXPECT().GetAll().Return(&mockBlogs, nil)
+			},
+		},
+		{
+			name:    "failure",
+			want:    nil,
+			wantErr: true,
+			prepareMockFn: func(m *mock_repo.MockRepo) {
+				m.EXPECT().GetAll().Return(nil, errors.New("err"))
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			repo := getMockRepo(t)
 			uc := &BlogPostUseCase{
-				repo: tt.fields.repo,
+				repo: repo,
 			}
+			tt.prepareMockFn(repo)
 			got, err := uc.GetAll()
 			if (err != nil) != tt.wantErr {
 				t.Errorf("GetAll() error = %v, wantErr %v", err, tt.wantErr)
@@ -87,26 +120,47 @@ func TestBlogPostUseCase_GetAll(t *testing.T) {
 }
 
 func TestBlogPostUseCase_GetOne(t *testing.T) {
-	type fields struct {
-		repo Repo
-	}
 	type args struct {
 		id string
 	}
+	mockBlog := getMockBlogPost("g")
 	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    *model.BlogPost
-		wantErr bool
+		name          string
+		args          args
+		want          *model.BlogPost
+		wantErr       bool
+		prepareMockFn func(m *mock_repo.MockRepo)
 	}{
-		// TODO: Add test cases.
+		{
+			name: "success",
+			args: args{
+				id: "1",
+			},
+			want:    mockBlog,
+			wantErr: false,
+			prepareMockFn: func(m *mock_repo.MockRepo) {
+				m.EXPECT().GetOne("1").Return(mockBlog, nil)
+			},
+		},
+		{
+			name: "failure",
+			args: args{
+				id: "invalid",
+			},
+			want:    nil,
+			wantErr: true,
+			prepareMockFn: func(m *mock_repo.MockRepo) {
+				m.EXPECT().GetOne("invalid").Return(nil, errors.New("err"))
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			repo := getMockRepo(t)
 			uc := &BlogPostUseCase{
-				repo: tt.fields.repo,
+				repo,
 			}
+			tt.prepareMockFn(repo)
 			got, err := uc.GetOne(tt.args.id)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("GetOne() error = %v, wantErr %v", err, tt.wantErr)
@@ -119,77 +173,77 @@ func TestBlogPostUseCase_GetOne(t *testing.T) {
 	}
 }
 
-func TestBlogPostUseCase_Post(t *testing.T) {
-	type fields struct {
-		repo Repo
-	}
-	type args struct {
-		post *model.BlogPost
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			uc := &BlogPostUseCase{
-				repo: tt.fields.repo,
-			}
-			if err := uc.Post(tt.args.post); (err != nil) != tt.wantErr {
-				t.Errorf("Post() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
-
-func TestBlogPostUseCase_Update(t *testing.T) {
-	type fields struct {
-		repo Repo
-	}
-	type args struct {
-		id      string
-		newdata *model.BlogPost
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			uc := &BlogPostUseCase{
-				repo: tt.fields.repo,
-			}
-			if err := uc.Update(tt.args.id, tt.args.newdata); (err != nil) != tt.wantErr {
-				t.Errorf("Update() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
-
-func TestNew(t *testing.T) {
-	type args struct {
-		db *gorm.DB
-	}
-	tests := []struct {
-		name string
-		args args
-		want *BlogPostUseCase
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := New(tt.args.db); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("New() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
+//func TestBlogPostUseCase_Post(t *testing.T) {
+//	type fields struct {
+//		repo Repo
+//	}
+//	type args struct {
+//		post *model.BlogPost
+//	}
+//	tests := []struct {
+//		name    string
+//		fields  fields
+//		args    args
+//		wantErr bool
+//	}{
+//		// TODO: Add test cases.
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			uc := &BlogPostUseCase{
+//				repo: tt.fields.repo,
+//			}
+//			if err := uc.Post(tt.args.post); (err != nil) != tt.wantErr {
+//				t.Errorf("Post() error = %v, wantErr %v", err, tt.wantErr)
+//			}
+//		})
+//	}
+//}
+//
+//func TestBlogPostUseCase_Update(t *testing.T) {
+//	type fields struct {
+//		repo Repo
+//	}
+//	type args struct {
+//		id      string
+//		newdata *model.BlogPost
+//	}
+//	tests := []struct {
+//		name    string
+//		fields  fields
+//		args    args
+//		wantErr bool
+//	}{
+//		// TODO: Add test cases.
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			uc := &BlogPostUseCase{
+//				repo: tt.fields.repo,
+//			}
+//			if err := uc.Update(tt.args.id, tt.args.newdata); (err != nil) != tt.wantErr {
+//				t.Errorf("Update() error = %v, wantErr %v", err, tt.wantErr)
+//			}
+//		})
+//	}
+//}
+//
+//func TestNew(t *testing.T) {
+//	type args struct {
+//		db *gorm.DB
+//	}
+//	tests := []struct {
+//		name string
+//		args args
+//		want *BlogPostUseCase
+//	}{
+//		// TODO: Add test cases.
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			if got := New(tt.args.db); !reflect.DeepEqual(got, tt.want) {
+//				t.Errorf("New() = %v, want %v", got, tt.want)
+//			}
+//		})
+//	}
+//}
