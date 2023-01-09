f7830863c4e340efe715a8e4177928f1dfb3cc05
OceanT-shirt <stylish.trousers@gmail.com>
2023-01-09 14:11:05 +0900

    edited models

--- home/api/getProducts.ts
+++ home/api/getProducts.ts
@@ -1,5 +1,10 @@
+import { db } from '@/lib/firestore';
+
+export const revalidate = 3600; // Management cache. See: https://beta.nextjs.org/docs/data-fetching/fetching#segment-cache-configuration
+
 async function getProducts() {
-  const res = await fetch('');
+  const snapshot = await db.collection('products').get();
+  snapshot.forEach((doc) => {});
 }
 
 export { getProducts };
--- home/lib/mocks.ts
+++ home/lib/mocks.ts
@@ -1,27 +1,4 @@
-type Product = {
-  title: MultiLangStr;
-  area: MultiLangStr;
-  text: MultiLangStr;
-  techTags: string[];
-  mediaUrls: MediaUrl[];
-  links: Link[];
-};
-
-type MultiLangStr = {
-  EN: string;
-  JA?: string;
-};
-
-type MediaUrl = {
-  alt: string;
-  type: 'img' | 'video';
-  url: string;
-};
-
-type Link = {
-  title: string;
-  url: string;
-};
+import { Product } from '../models/product';
 
 export const mocks: Product[] = [
   {
