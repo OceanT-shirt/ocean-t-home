33bea01ca137842665003185089ff37ad674d810
OceanT-shirt <stylish.trousers@gmail.com>
2023-01-12 00:24:58 +0900

    modified media design

--- home/ui/Molecules/MediaDisplay.tsx
+++ home/ui/Molecules/MediaDisplay.tsx
@@ -10,12 +10,12 @@ export const MediaDisplay = ({
   // TODO: add video player
   // TODO: add onclick
   return (
-    <Carousel duration={2000} className={'w-full'}>
+    <Carousel duration={2000} className={'h-full w-full'}>
       {mediaArray &&
         mediaArray.map((m) => {
           return (
             <div
-              className={'flex h-48 flex-col justify-center'}
+              className={'flex h-full flex-col justify-center'}
               key={mediaArray.indexOf(m)}
             >
               {m.type == 'img' && (
