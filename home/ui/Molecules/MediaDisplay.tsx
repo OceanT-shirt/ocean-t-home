ea733b8f886201d8d4e1254d3007dbfcc271dabc
OceanT-shirt <stylish.trousers@gmail.com>
2023-01-12 00:40:10 +0900

    modified products list

--- home/ui/Molecules/MediaDisplay.tsx
+++ home/ui/Molecules/MediaDisplay.tsx
@@ -10,7 +10,7 @@ export const MediaDisplay = ({
   // TODO: add video player
   // TODO: add onclick
   return (
-    <Carousel duration={2000} className={'h-full w-full'}>
+    <Carousel duration={3000} className={'h-full w-full'}>
       {mediaArray &&
         mediaArray.map((m) => {
           return (
@@ -18,8 +18,14 @@ export const MediaDisplay = ({
               className={'flex h-full flex-col justify-center'}
               key={mediaArray.indexOf(m)}
             >
-              {m.type == 'img' && (
+              {m.type == 'img' && m.url != '' ? (
                 <img src={m.url} alt={m.alt} className={'object-cover'} />
+              ) : (
+                <img
+                  src={'/no_image.png'}
+                  alt={'no image'}
+                  className={'object-cover'}
+                />
               )}
             </div>
           );
