81550d455ae738a2133614ff01fcf6b0c55c297f
OceanT-shirt <stylish.trousers@gmail.com>
2023-01-12 23:01:27 +0900

    edited design

--- home/ui/Organisms/Footer.tsx
+++ home/ui/Organisms/Footer.tsx
@@ -7,13 +7,13 @@ export const Footer = () => {
         <div className="text-sm text-zinc-600">
           Built with <b>Next.js 13</b>
           {', '}
-          deployed on <b>Cloud Run</b>
+          deployed on <b>App Engine</b>
           {'. '}
           <a
             className="underline decoration-dotted underline-offset-4"
             href="https://github.com/OceanT-shirt/ocean-t-home/tree/main/home"
           >
-            View the code
+            View the code / Submit issue (Welcome!)
           </a>
         </div>
       </div>
