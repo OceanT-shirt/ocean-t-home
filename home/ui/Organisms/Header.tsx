0db1e99feaf32ed16e5377af31cab41bfe760179
OceanT-shirt <stylish.trousers@gmail.com>
2023-01-15 16:00:06 +0900

    completed the header design

--- home/ui/Organisms/Header.tsx
+++ home/ui/Organisms/Header.tsx
@@ -7,6 +7,7 @@ import LocaleSwitcher from '@/ui/Molecules/LocaleSwitcher';
 import { FaGithub, FaLinkedin } from 'react-icons/all';
 import Link from 'next/link';
 import { IconButton } from '@/ui/Atoms/IconButton';
+import { useMediaSize } from '@/lib/use-media-size';
 
 const HeadContainer = styled.header`
   background: rgba(0, 57, 115, 0.7);
@@ -15,30 +16,23 @@ const HeadContainer = styled.header`
 `;
 
 export default function Header() {
+  const { isLG } = useMediaSize();
   return (
     <HeadContainer
-      className={
-        'relative flex h-16 flex-grow flex-col content-center items-center'
-      }
+      className={'relative flex flex-grow flex-col items-center justify-center'}
     >
-      <div className={'h-16 items-center'}>
-        <HeadingText kind={'h1'} className={'my-auto'}>
-          Haruka Takahira
-        </HeadingText>
+      <div className={'mx-auto my-4'}>
+        <HeadingText kind={'h1'}>Haruka Takahira</HeadingText>
       </div>
-      {/*<div*/}
-      {/*  className={*/}
-      {/*    'r-6 absolute flex flex-row items-center gap-x-4 justify-self-end'*/}
-      {/*  }*/}
-      {/*>*/}
-      {/*  <Link href={process.env.NEXT_PUBLIC_LINKEDIN_URL ?? ''}>*/}
-      {/*    <IconButton icon={FaLinkedin} />*/}
-      {/*  </Link>*/}
-      {/*  <Link href={process.env.NEXT_PUBLIC_GITHUB_URL ?? ''}>*/}
-      {/*    <IconButton icon={FaGithub} />*/}
-      {/*  </Link>*/}
-      {/*  <LocaleSwitcher />*/}
-      {/*</div>*/}
+      <div className={'absolute right-6 flex flex-row items-center gap-x-4'}>
+        <Link href={process.env.NEXT_PUBLIC_LINKEDIN_URL ?? ''}>
+          <IconButton icon={FaLinkedin} />
+        </Link>
+        <Link href={process.env.NEXT_PUBLIC_GITHUB_URL ?? ''}>
+          <IconButton icon={FaGithub} />
+        </Link>
+        <LocaleSwitcher isCompact={!isLG} />
+      </div>
     </HeadContainer>
   );
 }
