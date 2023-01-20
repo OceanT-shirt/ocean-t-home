f22cf680e68e1013848310ef5deeadff234610af
OceanT-shirt <stylish.trousers@gmail.com>
2023-01-20 22:42:35 +0900

    modified: taglist overflow control

--- home/ui/Molecules/TagList.tsx
+++ home/ui/Molecules/TagList.tsx
@@ -2,7 +2,7 @@ import { Tag } from '@/ui/Atoms/Tag';
 
 export const TagList = ({ tags }: { tags: string[] }) => {
   return (
-    <div className={'flex flex-row space-x-2'}>
+    <div className={'flex flex-row space-x-2 overflow-x-auto'}>
       {tags.map((tag) => (
         <Tag text={tag} kind={'default'} key={tags.indexOf(tag)} />
       ))}
